a1a339d9cbe34a034bbc6b4afe53fe646f953016
Haris Suljić <imemesputava@gmail.com>
2019-03-19 13:21:36 +0100

    DE for the server
    This one can reach 200k in 150 gens.

--- agents/Config.py
+++ agents/Config.py
@@ -1,9 +1,9 @@
-pop_size              = 150
+pop_size              = 150 #has to be >=10
 
 # network parameters
 num_inputs              = 12
 num_outputs             = 2
-nodes_per_layer = [9, 6, 3]
+nodes_per_layer = [8, 4]
 num_hidden              = len(nodes_per_layer)
 
 
@@ -16,10 +16,10 @@ weight_mutate_power     = 0.5
 weight_mutate_rate      = 0.8
 weight_replace_rate     = 0.1
 
-number_of_generations = 500
+number_of_generations = 150
 mutation_probability = 0.01
-mutation_rate = 1.5
-crossp = 0.7
+mutation_rate = 0.8
+crossp = 0.5
 elitism_portion = 0.3
 random_others_portion = 0.2
 currentToPBest = 0.1
